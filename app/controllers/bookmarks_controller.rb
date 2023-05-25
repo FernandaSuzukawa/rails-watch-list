f8b63d267134e6c3c16b52c86848ad9e594e174d
fernanda suzukawa <fernandasuzukawa@hotmail.com>
2023-05-25 18:39:28 -0300

    fjisdodfj

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -22,6 +22,7 @@ class BookmarksController < ApplicationController
   def create
     @bookmark = Bookmark.new(bookmark_params)
     @list = List.find(params[:list_id])
+    @bookmark.list = @list
 
     if @bookmark.save
       redirect_to list_path(@list)
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
-  resourses :lists, except: %i[edit update] do
+  resources :lists, except: %i[edit update] do
     resources :bookmarks, only: %i[new create]
   end
   resources :bookmarks, only: %i[destroy]
